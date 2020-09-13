7a87aaef0533ebb9f5915b5bcfcbe0489848e85e
Arashi <crdurnin15@gmail.com>
2020-09-13 12:27:00 -0700

    Update maingame.py

--- maingame.py
+++ maingame.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -14,25 +15,26 @@ walkLeft = [pygame.image.load('NinjaSprite/L1.png'),
             pygame.image.load('NinjaSprite/L2.png'),
             pygame.image.load('NinjaSprite/L3.png'),
             pygame.image.load('NinjaSprite/L4.png')]
-walkUp = [pygame.image.load('NinjaSprite/F1.png'),
-          pygame.image.load('NinjaSprite/F2.png'),
-          pygame.image.load('NinjaSprite/F3.png'),
-          pygame.image.load('NinjaSprite/F4.png')]
-walkDown = [pygame.image.load('NinjaSprite/B1.png'),
-            pygame.image.load('NinjaSprite/B2.png'),
-            pygame.image.load('NinjaSprite/B3.png'),
-            pygame.image.load('NinjaSprite/B4.png')]
+walkUp = [pygame.image.load('NinjaSprite/U1.png'),
+          pygame.image.load('NinjaSprite/U2.png'),
+          pygame.image.load('NinjaSprite/U3.png'),
+          pygame.image.load('NinjaSprite/U4.png')]
+walkDown = [pygame.image.load('NinjaSprite/D1.png'),
+            pygame.image.load('NinjaSprite/D2.png'),
+            pygame.image.load('NinjaSprite/D3.png'),
+            pygame.image.load('NinjaSprite/D4.png')]
 jumpRight = pygame.image.load('NinjaSprite/RJ.png')
 jumpLeft = pygame.image.load('NinjaSprite/LJ.png')
-jumpUp = pygame.image.load('NinjaSprite/Back.png')
-jumpDown = pygame.image.load('NinjaSprite/Standing.png')
+jumpUp = pygame.image.load('NinjaSprite/UJ.png')
+jumpUp2 = pygame.image.load('NinjaSprite/UJ_2.png')
+jumpDown = pygame.image.load('NinjaSprite/DJ.png')
 bg = pygame.image.load('BGMazes/maze-(1).png')
 char = pygame.image.load('NinjaSprite/Standing.png')
 char_back = pygame.image.load('NinjaSprite/Back.png')
 clock = pygame.time.Clock()
 
 
-class player(object):
+class player (object):
     def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
@@ -41,51 +43,53 @@ class player(object):
         self.vel = 5
         self.isJump = False
         self.jumpCount = 10
-        self.left = False
-        self.right = False
         self.up = False
         self.down = False
-        self.LB = False
+        self.left = False
+        self.right = False
+        self.back = 0
+
 
     def draw(self, win):
         if self.walkCount + 1 >= 8:
             self.walkCount = 0
+        if self.walkCount >= 1:
+            self.back = 0
+
         # Y COORDINATE MOVES
-        if self.up:
-            win.blit(walkUp[p1.walkCount // 2], (round(self.x), round(self.y)))
+        if self.up and not self.isJump:
+            win.blit(walkUp[self.walkCount // 2], (round(self.x), round(self.y)))
             self.walkCount += 1
-        elif self.down:
-            win.blit(walkDown[p1.walkCount // 2], (round(self.x), round(self.y)))
+            self.back += 1
+        elif self.down and not self.isJump:
+            win.blit(walkDown[self.walkCount // 2], (round(self.x), round(self.y)))
             self.walkCount += 1
+
         # X COORDINATE MOVES
-        elif self.left:
-            win.blit(walkLeft[p1.walkCount // 2], (round(self.x), round(self.y)))
+        elif self.left and not self.isJump:
+            win.blit(walkLeft[self.walkCount // 2], (round(self.x), round(self.y)))
             self.walkCount += 1
-        elif self.right:
-            win.blit(walkRight[p1.walkCount // 2], (round(self.x), round(self.y)))
+        elif self.right and not self.isJump:
+            win.blit(walkRight[self.walkCount // 2], (round(self.x), round(self.y)))
             self.walkCount += 1
+
+        # IDLE POSITIONS
         else:
-            if self.LB:
-                win.blit(char_back, (round(self.x), round(self.y)))
-                self.walkCount = 0
-
-            else:
-                win.blit(char, (round(self.x), round(self.y)))
-                self.walkCount = 0
-
+            if not self.isJump:
+                if self.back >= 1:
+                    win.blit(char_back, (round(self.x), round(self.y)))
+                else:
+                    win.blit(char, (round(self.x), round(self.y)))
+        # JUMPING
         if self.isJump:
             if self.up:
-                win.blit(jumpUp[p1.walkCount // 2] (round(self.x), round(self.y)))
-                self.walkCount += 1
+                win.blit(jumpUp, (round(self.x), round(self.y)))
             elif self.down:
-                win.blit(jumpDown[p1.walkCount // 2] (round(self.x), round(self.y)))
-                self.walkCount += 1
+                win.blit(jumpDown, (round(self.x), round(self.y)))
             elif self.left:
-                win.blit(jumpLeft[p1.walkCount // 2] (round(self.x), round(self.y)))
-                self.walkCount += 1
+                win.blit(jumpLeft, (round(self.x), round(self.y)))
             elif self.right:
-                win.blit(jumpRight[p1.walkCount // 2] (round(self.x), round(self.y)))
-                self.walkCount += 1
+                win.blit(jumpRight, (round(self.x), round(self.y)))
             else:
                 win.blit(char, (round(self.x), round(self.y)))
 
@@ -115,7 +119,6 @@ while run:
         p1.right = False
         p1.up = False
         p1.down = False
-
     elif keys[pygame.K_RIGHT] and p1.x < 1440 - p1.width - p1.vel:
         p1.x += p1.vel
         p1.left = False
@@ -130,27 +133,6 @@ while run:
         p1.down = False
         p1.left = False
         p1.right = False
-        for event in pygame.event.get():
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_UP:
-                    LB = True
-                    while True:
-                        LB = True
-                        if p1.walkCount <= 1:
-                            break
-                        else:
-                            LB = True
-
-        # ******
-        # what i want to happen is when the up arrow key is released it will show
-        # the players back. but i can't seem to figure out how to do that. currently it just goes back
-        # to the players front facing position. the players backs connected to the char_back variable.
-        # you can see above in asterisks idle positions that hold both the front and back facing variables.
-        # what i thought could work is a while loop where it detects the up arrow key release
-        # making LB (looking back) = True and then while true it would display the players back. once the walkCount
-        # was greater than or = to 1 the loop would break. any help is greatly appreciated.
-
-
     elif keys[pygame.K_DOWN] and p1.y < 800 - p1.vel - p1.width:
         p1.y += p1.vel
         p1.up = False
@@ -170,7 +152,6 @@ while run:
     if not p1.isJump:
         if keys[pygame.K_SPACE]:
             p1.isJump = True
-            p1.walkCount = 0
     else:
         if p1.jumpCount >= -10:
             p1.y -= (p1.jumpCount * abs(p1.jumpCount)) * 0.1
